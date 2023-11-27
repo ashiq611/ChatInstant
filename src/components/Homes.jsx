064668689155b72f8b6b69fe15c831495decf994
ashiq611 <72414858+ashiq611@users.noreply.github.com>
2023-11-27 23:38:34 +0600

    profile uplaod design done

--- src/components/Homes.jsx
+++ src/components/Homes.jsx
@@ -8,30 +8,32 @@ import BlockedUsers from "./BlockedUsers";
 
 const Homes = () => {
   return (
-    <div className="h-screen w-full lg:flex justify-between flex-wrap">
-      <label
-        htmlFor="my-drawer-2"
-        className="btn btn-primary btn-sm drawer-button text-2xl lg:hidden"
-      >
-        <MdOutlineMenuOpen />
-      </label>
-      <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
-        <GroupList />
-      </div>
-      <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
-        <Friends />
-      </div>
-      <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
-        <UserList />
-      </div>
-      <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
-        <FriendRequest />
-      </div>
-      <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
-        <MyGroups />
-      </div>
-      <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
-        <BlockedUsers />
+    <div>
+      <div className="h-screen w-full lg:flex justify-between flex-wrap">
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary btn-sm drawer-button text-2xl lg:hidden sticky top-0 left-0 z-30"
+        >
+          <MdOutlineMenuOpen />
+        </label>
+        <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
+          <GroupList />
+        </div>
+        <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
+          <Friends />
+        </div>
+        <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
+          <UserList />
+        </div>
+        <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
+          <FriendRequest />
+        </div>
+        <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
+          <MyGroups />
+        </div>
+        <div className="w-full mt-2 lg:w-[33%] h-1/2 shadow-lg shadow-base-300 list overflow-y-scroll">
+          <BlockedUsers />
+        </div>
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,90 +11,117 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { userLoginInfo } from "../../slices/userSlice";
 import { toast } from "react-toastify";
-
-
+import { FaCloudUploadAlt } from "react-icons/fa";
 
 const Home = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const data = useSelector((state) => state.userLoginInfo.userInfo)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.userLoginInfo.userInfo);
 
-  // private page 
+  // private page
   useEffect(() => {
-    if(!data) {
-      navigate('/')
+    if (!data) {
+      navigate("/");
     }
-  })
+  });
 
   const logOutHandle = () => {
-    localStorage.removeItem("user")
-    dispatch(userLoginInfo(null))
-    navigate('/')
+    localStorage.removeItem("user");
+    dispatch(userLoginInfo(null));
+    navigate("/");
     toast.warn("Logged out Successfully");
-  }
-    return (
-      <div>
-        <div className="drawer lg:drawer-open">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="h-screen w-full  drawer-content flex flex-col items-center justify-center">
-            {/* Page content here */}
-            <Homes />
+  };
+  return (
+    <div>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="h-screen w-full  drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <Homes />
+          {/* profile upload modal */}
+          <div>
+            <dialog id="my_modal_1" className="modal">
+              <div className="modal-box">
+                <h3 className="font-bold text-lg">Upload Your Profile Picture</h3>
+                <p className="py-4">
+                  Press ESC key or click the button below to close
+                </p>
+                <div className="modal-action">
+                  <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
           </div>
-          <div className="drawer-side z-30">
+        </div>
+        <div className="drawer-side z-30">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <div className="relative w-40 min-h-full bg-gradient-to-r from-[#8d99ae] [#edf2f4] to-[#ffffff] text-base-content flex flex-col justify-between">
             <label
               htmlFor="my-drawer-2"
-              aria-label="close sidebar"
-              className="drawer-overlay"
-            ></label>
-            <div className="relative w-40 min-h-full bg-gradient-to-r from-[#8d99ae] [#edf2f4] to-[#ffffff] text-base-content flex flex-col justify-between">
-              <label
-                htmlFor="my-drawer-2"
-                className="absolute top-0.5 right-0.5 btn drawer-button lg:hidden"
-              >
-                <ImCross />
-              </label>
+              className="absolute top-0.5 right-0.5 btn drawer-button lg:hidden"
+            >
+              <ImCross />
+            </label>
 
-              <ul className="menu p-4 pt-10 w-40">
-                <li>
-                  <div className="avatar online">
-                    <div className="w-24 rounded-full">
-                      <img src={data?.photoURL} />
+            <ul className="menu p-4 pt-10 w-40">
+              <li>
+                <div className="avatar online">
+                  <div className="w-24 rounded-full relative group">
+                    <img src={data?.photoURL} />
+                    <div
+                      className="uploadIcon h-full w-full hidden group-hover:flex absolute left-0 top-0 justify-center items-center text-3xl text-yellow-500 bg-black bg-opacity-70 cursor-pointer"
+                      onClick={() =>
+                        document.getElementById("my_modal_1").showModal()
+                      }
+                    >
+                      <FaCloudUploadAlt />
                     </div>
                   </div>
-                </li>
-              </ul>
-              <ul className="menu p-4 w-30 flex flex-col items-center gap-8 font-bold">
-                <li>
-                  <a>
-                    <Lottie animationData={homeLogoAni} />
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    {" "}
-                    <Lottie animationData={inboxLogoAni} />
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    <Lottie animationData={notiLogoAni} />
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    <Lottie animationData={settingsLogoAni} />
-                  </a>
-                </li>
-              </ul>
-              <ul className="menu p-4 w-30">
-                <li>
-                  <button onClick={logOutHandle}>
-                    <Lottie animationData={logoutLogoAni} />
-                  </button>
-                </li>
-              </ul>
-            </div>
-            {/* <ul className="menu p-4 w-40 min-h-full bg-base-200 text-base-content">
+                </div>
+                <div className="flex justify-center">
+                  <h2 className="font-bold capitalize">{data?.displayName}</h2>
+                </div>
+              </li>
+            </ul>
+            <ul className="menu p-4 w-30 flex flex-col items-center gap-8 font-bold">
+              <li>
+                <a>
+                  <Lottie animationData={homeLogoAni} />
+                </a>
+              </li>
+              <li>
+                <a>
+                  {" "}
+                  <Lottie animationData={inboxLogoAni} />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Lottie animationData={notiLogoAni} />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Lottie animationData={settingsLogoAni} />
+                </a>
+              </li>
+            </ul>
+            <ul className="menu p-4 w-30">
+              <li>
+                <button onClick={logOutHandle}>
+                  <Lottie animationData={logoutLogoAni} />
+                </button>
+              </li>
+            </ul>
+          </div>
+          {/* <ul className="menu p-4 w-40 min-h-full bg-base-200 text-base-content">
              
               <li>
                 <div className="avatar online">
@@ -119,10 +146,10 @@ const Home = () => {
                 <a>Logout</a>
               </li>
             </ul>  */}
-          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
